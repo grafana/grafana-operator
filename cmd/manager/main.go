ef2107160a8e0f237b9344c1e3098851612dacc3
Peter Braun <pbraun@redhat.com>
2019-04-24 12:43:03 +0200

    update image repo name

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -22,6 +22,8 @@ import (
 )
 
 var log = logf.Log.WithName("cmd")
+var flagImage string
+var flagImageTag string
 
 func printVersion() {
 	log.Info(fmt.Sprintf("Go Version: %s", runtime.Version()))
@@ -29,11 +31,14 @@ func printVersion() {
 	log.Info(fmt.Sprintf("operator-sdk Version: %v", sdkVersion.Version))
 }
 
-func main() {
-	flagImage := flag.String("grafana-image", "", "Overrides the default Grafana image")
-	flagImageTag := flag.String("grafana-image-tag", "", "Overrides the default Grafana image tag")
-	flag.Parse()
+func init() {
+	flagset := flag.CommandLine
+	flagset.StringVar(&flagImage, "grafana-image", "", "Overrides the default Grafana image")
+	flagset.StringVar(&flagImageTag, "grafana-image-tag", "", "Overrides the default Grafana image tag")
+	flagset.Parse(os.Args[1:])
+}
 
+func main() {
 	// The logger instantiated here can be changed to any logger
 	// implementing the logr.Logger interface. This logger will
 	// be propagated through the whole operator, generating
@@ -44,8 +49,8 @@ func main() {
 
 	// Controller configuration
 	controllerConfig := grafana.GetControllerConfig()
-	controllerConfig.AddConfigItem(grafana.ConfigGrafanaImage, *flagImage)
-	controllerConfig.AddConfigItem(grafana.ConfigGrafanaImageTag, *flagImageTag)
+	controllerConfig.AddConfigItem(grafana.ConfigGrafanaImage, flagImage)
+	controllerConfig.AddConfigItem(grafana.ConfigGrafanaImageTag, flagImageTag)
 
 	namespace, err := k8sutil.GetWatchNamespace()
 	if err != nil {
