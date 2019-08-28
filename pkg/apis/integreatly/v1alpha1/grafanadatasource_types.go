25fdbb29447e292f11fed66a9d33e4c7108b800b
Peter Braun <pbraun@redhat.com>
2019-08-28 13:08:07 +0200

    fix datasource json and secure json fields

--- pkg/apis/integreatly/v1alpha1/grafanadatasource_types.go
+++ pkg/apis/integreatly/v1alpha1/grafanadatasource_types.go
@@ -48,23 +48,63 @@ type GrafanaDataSourceList struct {
 }
 
 type GrafanaDataSourceFields struct {
-	Name              string            `json:"name"`
-	Type              string            `json:"type"`
-	Access            string            `json:"access"`
-	OrgId             int               `json:"orgId,omitempty"`
-	Url               string            `json:"url"`
-	Password          string            `json:"password,omitempty"`
-	User              string            `json:"user,omitempty"`
-	Database          string            `json:"database,omitempty"`
-	BasicAuth         bool              `json:"basicAuth,omitempty"`
-	BasicAuthUser     string            `json:"basicAuthUser,omitempty"`
-	BasicAuthPassword string            `json:"basicAuthPassword,omitempty"`
-	WithCredentials   bool              `json:"withCredentials,omitempty"`
-	IsDefault         bool              `json:"isDefault,omitempty"`
-	JsonData          map[string]string `json:"jsonData,omitempty"`
-	SecureJsonData    map[string]string `json:"secureJsonData,omitempty"`
-	Version           int               `json:"version,omitempty"`
-	Editable          bool              `json:"editable,omitempty"`
+	Name              string                          `json:"name"`
+	Type              string                          `json:"type"`
+	Access            string                          `json:"access"`
+	OrgId             int                             `json:"orgId,omitempty"`
+	Url               string                          `json:"url"`
+	Password          string                          `json:"password,omitempty"`
+	User              string                          `json:"user,omitempty"`
+	Database          string                          `json:"database,omitempty"`
+	BasicAuth         bool                            `json:"basicAuth,omitempty"`
+	BasicAuthUser     string                          `json:"basicAuthUser,omitempty"`
+	BasicAuthPassword string                          `json:"basicAuthPassword,omitempty"`
+	WithCredentials   bool                            `json:"withCredentials,omitempty"`
+	IsDefault         bool                            `json:"isDefault,omitempty"`
+	JsonData          GrafanaDataSourceJsonData       `json:"jsonData,omitempty"`
+	SecureJsonData    GrafanaDataSourceSecureJsonData `json:"secureJsonData,omitempty"`
+	Version           int                             `json:"version,omitempty"`
+	Editable          bool                            `json:"editable,omitempty"`
+}
+
+// The most common json options
+// See https://grafana.com/docs/administration/provisioning/#datasources
+type GrafanaDataSourceJsonData struct {
+	TlsAuth                 bool   `json:"tlsAuth"`
+	TlsAuthWithCACert       bool   `json:"tlsAuthWithCACert"`
+	TlsSkipVerify           bool   `json:"tlsSkipVerify"`
+	GraphiteVersion         string `json:"graphiteVersion"`
+	TimeInterval            string `json:"timeInterval"`
+	EsVersion               int    `json:"esVersion"`
+	TimeField               string `json:"timeField"`
+	Interval                string `json:"interval"`
+	LogMessageField         string `json:"logMessageField"`
+	LogLevelField           string `json:"logLevelField"`
+	AuthType                string `json:"authType"`
+	AssumeRoleArn           string `json:"assumeRoleArn"`
+	DefaultRegion           string `json:"defaultRegion"`
+	CustomMetricsNamespaces string `json:"customMetricsNamespaces"`
+	TsdbVersion             string `json:"tsdbVersion"`
+	TsdbResolution          string `json:"tsdbResolution"`
+	Sslmode                 string `json:"sslmode"`
+	Encrypt                 string `json:"encrypt"`
+	PostgresVersion         int    `json:"postgresVersion"`
+	Timescaledb             bool   `json:"timescaledb"`
+	MaxOpenConns            int    `json:"maxOpenConns"`
+	MaxIdleConns            int    `json:"maxIdleConns"`
+	ConnMaxLifetime         int    `json:"connMaxLifetime"`
+}
+
+// The most common secure json options
+// See https://grafana.com/docs/administration/provisioning/#datasources
+type GrafanaDataSourceSecureJsonData struct {
+	TlsCaCert         string `json:"tlsCACert"`
+	TlsClientCert     string `json:"tlsClientCert"`
+	TlsClientKey      string `json:"tlsClientKey"`
+	Password          string `json:"password"`
+	BasicAuthPassword string `json:"basicAuthPassword"`
+	AccessKey         string `json:"accessKey"`
+	SecretKey         string `json:"secretKey"`
 }
 
 func init() {
