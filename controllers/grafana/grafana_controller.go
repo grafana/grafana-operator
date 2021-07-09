grafana/grafana-operator

commit 1717e642433558cb3b968ef5b92029bfbe8ed6bd

fix: use correct service dns name

controllers/grafana/grafana_controller.go
@@ -262,7 +262,7 @@ func (r *ReconcileGrafana) getGrafanaAdminUrl(cr *grafanav1alpha1.Grafana, state
 
 	// Otherwise rely on the service
 	if state.GrafanaService != nil {
-		return fmt.Sprintf("http://%v:%d", state.GrafanaService.Name,
+		return fmt.Sprintf("%v.%v.svc.cluster.local:%d", state.GrafanaService.Name, cr.Namespace,
 			servicePort), nil
 	}
 

commit 639d463ce46963e9afdc9e771596db6b897cc770

Fix updating dashboards
Fixes this issue in logs:

```
{"level":"error","ts":1625826200.368101,"logger":"controller_grafanadashboard","msg":"failed to get or create namespace folder General for dashboard ","error":"Get grafana-service.monitoring.svc.cluster.local:3000/api/folders: unsupported protocol scheme \"grafana-service.monitoring.svc.cluster.local\"","stacktrace":"github.com/go-logr/zapr.(*zapLogger).Error\n\tpkg/mod/github.com/go-logr/zapr@v0.1.1/zapr.go:128\ngithub.com/integr8ly/grafana-operator/v3/pkg/controller/grafanadashboard.(*ReconcileGrafanaDashboard).reconcileDashboards\n\tsrc/github.com/integr8ly/grafana-operator/pkg/controller/grafanadashboard/dashboard_controller.go:253\ngithub.com/integr8ly/grafana-operator/v3/pkg/controller/grafanadashboard.(*ReconcileGrafanaDashboard).Reconcile\n\tsrc/github.com/integr8ly/grafana-operator/pkg/controller/grafanadashboard/dashboard_controller.go:136\ngithub.com/integr8ly/grafana-operator/v3/pkg/controller/grafanadashboard.add.func1\n\tsrc/github.com/integr8ly/grafana-operator/pkg/controller/grafanadashboard/dashboard_controller.go:86\ngithub.com/integr8ly/grafana-operator/v3/pkg/controller/grafanadashboard.add.func2\n\tsrc/github.com/integr8ly/grafana-operator/pkg/controller/grafanadashboard/dashboard_controller.go:92"}
{"level":"error","ts":1625826200.368165,"logger":"controller_grafanadashboard","msg":"error updating dashboard","error":"Get grafana-service.monitoring.svc.cluster.local:3000/api/folders: unsupported protocol scheme \"grafana-service.monitoring.svc.cluster.local\"","stacktrace":"github.com/go-logr/zapr.(*zapLogger).Error\n\tpkg/mod/github.com/go-logr/zapr@v0.1.1/zapr.go:128\ngithub.com/integr8ly/grafana-operator/v3/pkg/controller/grafanadashboard.(*ReconcileGrafanaDashboard).manageError\n\tsrc/github.com/integr8ly/grafana-operator/pkg/controller/grafanadashboard/dashboard_controller.go:366\ngithub.com/integr8ly/grafana-operator/v3/pkg/controller/grafanadashboard.(*ReconcileGrafanaDashboard).reconcileDashboards\n\tsrc/github.com/integr8ly/grafana-operator/pkg/controller/grafanadashboard/dashboard_controller.go:254\ngithub.com/integr8ly/grafana-operator/v3/pkg/controller/grafanadashboard.(*ReconcileGrafanaDashboard).Reconcile\n\tsrc/github.com/integr8ly/grafana-operator/pkg/controller/grafanadashboard/dashboard_controller.go:136\ngithub.com/integr8ly/grafana-operator/v3/pkg/controller/grafanadashboard.add.func1\n\tsrc/github.com/integr8ly/grafana-operator/pkg/controller/grafanadashboard/dashboard_controller.go:86\ngithub.com/integr8ly/grafana-operator/v3/pkg/controller/grafanadashboard.add.func2\n\tsrc/github.com/integr8ly/grafana-operator/pkg/controller/grafanadashboard/dashboard_controller.go:92"}
```

Was broken with https://github.com/grafana-operator/grafana-operator/commit/1717e642433558cb3b968ef5b92029bfbe8ed6bd

controllers/grafana/grafana_controller.go
@@ -263,7 +263,7 @@ func (r *ReconcileGrafana) getGrafanaAdminUrl(cr *grafanav1alpha1.Grafana, state
 
 	// Otherwise rely on the service
 	if state.GrafanaService != nil {
-		return fmt.Sprintf("%v.%v.svc.cluster.local:%d", state.GrafanaService.Name, cr.Namespace,
+		return fmt.Sprintf("http://%v.%v.svc.cluster.local:%d", state.GrafanaService.Name, cr.Namespace,
 			servicePort), nil
 	}
 