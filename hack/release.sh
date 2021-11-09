0f0444f99ae45122ccf8e329691a3a7528404ab3
Edvin Norling <edvin.norling@xenit.se>
2021-11-09 22:53:51 +0100

    Add kustomization file in BASE_PATH
    Adding namespace in manifests.

--- hack/release.sh
+++ hack/release.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-# This script is used to generate a tagged version if the grafana-oeprator yaml
+# This script is used to generate a tagged version if the grafana-operator yaml
 # This way users can easily use the pre-built kustomization file or setup there own but be able to point on these artifacts.
 
-TAG="v4.0.1"
+#TAG="v4.0.2"
+TAG="latest"
 if [[ $1 != "" ]]; then
   TAG=$1
 fi
@@ -20,3 +21,6 @@ BASE_PATH=./deploy/manifests/$TAG/
 mkdir -p $BASE_PATH
 kustomize build ./config/crd > $BASE_PATH/crds.yaml
 kustomize build ./config/manager > $BASE_PATH/deployment.yaml
+
+cd $BASE_PATH
+kustomize create --autodetect
