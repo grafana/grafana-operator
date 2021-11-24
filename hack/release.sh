0dd93bbf2fbb7ff6b7b1d89357f2b6235cdb3b34
Edvin Norling <edvin.norling@xenit.se>
2021-11-24 21:26:55 +0100

    Add -ex to bash script

--- hack/release.sh
+++ hack/release.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -ex
 
 # This script is used to generate a tagged version if the grafana-operator yaml
 # This way users can easily use the pre-built kustomization file or setup there own but be able to point on these artifacts.
