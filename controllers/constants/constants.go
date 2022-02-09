0af592b78d67bb82c613e178260226667d5481a5
Edvin N <edvin.norling@xenit.se>
2022-02-09 20:41:59 +0100

    Upgrade default grafana version to 7.5.15 (#679)

--- controllers/constants/constants.go
+++ controllers/constants/constants.go
@@ -2,7 +2,7 @@ package constants
 
 const (
 	GrafanaImage                            = "docker.io/grafana/grafana"
-	GrafanaVersion                          = "7.5.11"
+	GrafanaVersion                          = "7.5.15"
 	GrafanaServiceAccountName               = "grafana-serviceaccount"
 	GrafanaServiceName                      = "grafana-service"
 	GrafanaDataStorageName                  = "grafana-pvc"
