0f0e7d11f98559f8b7099225d1f2f13c278986db
Andreas Mårtensson <andreas@addem.se>
2022-03-24 21:21:22 +0100

    Track dashboards per namespace in ControllerConfig (#690)
    * Track dashboards per namespace in ControllerConfig
    
    * Remove extra line in generated code
    
    * Fix status update for the Grafana resource
    
    Co-authored-by: Edvin N <edvin.norling@xenit.se>
    Co-authored-by: Hubert Stefanski <35736504+HubertStefanski@users.noreply.github.com>

--- controllers/grafana/grafana_controller.go
+++ controllers/grafana/grafana_controller.go
@@ -283,11 +283,7 @@ func (r *ReconcileGrafana) manageSuccess(cr *grafanav1alpha1.Grafana, state *com
 	// Only update the status if the dashboard controller had a chance to sync the cluster
 	// dashboards first. Otherwise reuse the existing dashboard config from the CR.
 	if r.Config.GetConfigBool(config.ConfigGrafanaDashboardsSynced, false) {
-		cr.Status.InstalledDashboards = r.Config.Dashboards
-	} else {
-		if r.Config.Dashboards == nil {
-			r.Config.SetDashboards([]*grafanav1alpha1.GrafanaDashboardRef{})
-		}
+		cr.Status.InstalledDashboards = r.Config.GetAllDashboards()
 	}
 
 	instance := &grafanav1alpha1.Grafana{}
