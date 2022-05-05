bf9b70d6a859efe97507e19cd3f29e9672f4e7d0
gitgaoxiang <gao_xiang@126.com>
2022-05-05 16:09:04 +0800

    Update grafanaService.go
    format arrangement

--- controllers/model/grafanaService.go
+++ controllers/model/grafanaService.go
@@ -68,9 +68,9 @@ func GetGrafanaPort(cr *v1alpha1.Grafana) int {
 }
 
 func getServicePorts(cr *v1alpha1.Grafana, currentState *v1.Service) []v1.ServicePort {
-	intPort := int32(GetGrafanaPort(cr))	
+	intPort := int32(GetGrafanaPort(cr))
 	nodePort := int32(0)
-	for _,nPort := range cr.Spec.Service.Ports{
+	for _, nPort := range cr.Spec.Service.Ports {
 		if nPort.Port == 3000 {
 			nodePort = nPort.NodePort
 		}
@@ -81,7 +81,7 @@ func getServicePorts(cr *v1alpha1.Grafana, currentState *v1.Service) []v1.Servic
 			Protocol:   "TCP",
 			Port:       intPort,
 			TargetPort: intstr.FromString("grafana-http"),
-			NodePort: nodePort,
+			NodePort:   nodePort,
 		},
 	}
 
