3dd7eb6d96942dd658de3ce0e89a60f091ef52e7
Igor Beliakov <demtis.register@gmail.com>
2022-07-20 18:55:25 +0200

    Slightly rewrite the switch
    Signed-off-by: Igor Beliakov <demtis.register@gmail.com>

--- controllers/grafana/grafana_controller.go
+++ controllers/grafana/grafana_controller.go
@@ -275,13 +275,13 @@ func (r *ReconcileGrafana) getGrafanaAdminUrl(cr *grafanav1alpha1.Grafana, state
 		protocol := "http"
 
 		if cr.Spec.Config.Server != nil {
-			switch protocol = cr.Spec.Config.Server.Protocol; protocol {
+			switch cr.Spec.Config.Server.Protocol {
 			case "", "http":
 				protocol = "http"
 			case "https":
 				protocol = "https"
 			default:
-				return "", fmt.Errorf("server protocol %v is not supported, please use either http or https", protocol)
+				return "", fmt.Errorf("server protocol %v is not supported, please use either http or https", cr.Spec.Config.Server.Protocol)
 			}
 		}
 
