350dabc4321d34d788ebf16bac0f860476153101
Edvin Norling <edvin.norling@xenit.se>
2022-12-11 11:53:20 +0100

    Use signal.NotifyContext for easier stop solution.

--- main.go
+++ main.go
@@ -17,10 +17,10 @@ limitations under the License.
 package main
 
 import (
+	"context"
 	"flag"
 	"os"
 	"os/signal"
-	"syscall"
 
 	discovery2 "k8s.io/client-go/discovery"
 
@@ -79,16 +79,8 @@ func main() {
 		setupLog.Info("operator restricted to namespace", "namespace", namespace)
 	}
 
-	stop := make(chan bool)
-	sigs := make(chan os.Signal)
-	signal.Notify(sigs, syscall.SIGTERM, syscall.SIGABRT, syscall.SIGINT)
-	go func() {
-		sig := <-sigs
-		switch sig {
-		case syscall.SIGTERM, syscall.SIGABRT, syscall.SIGINT:
-			close(stop)
-		}
-	}()
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
 
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
 		Namespace:              namespace,
@@ -116,7 +108,7 @@ func main() {
 		Client: mgr.GetClient(),
 		Scheme: mgr.GetScheme(),
 		Log:    ctrl.Log,
-	}).SetupWithManager(mgr, stop); err != nil {
+	}).SetupWithManager(mgr, ctx); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "GrafanaDashboard")
 		os.Exit(1)
 	}
@@ -124,14 +116,14 @@ func main() {
 		Client: mgr.GetClient(),
 		Scheme: mgr.GetScheme(),
 		Log:    ctrl.Log,
-	}).SetupWithManager(mgr, stop); err != nil {
+	}).SetupWithManager(mgr, ctx); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "GrafanaDatasource")
 		os.Exit(1)
 	}
 	if err = (&controllers.GrafanaFolderReconciler{
 		Client: mgr.GetClient(),
 		Scheme: mgr.GetScheme(),
-	}).SetupWithManager(mgr, stop); err != nil {
+	}).SetupWithManager(mgr, ctx); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "GrafanaFolder")
 		os.Exit(1)
 	}
