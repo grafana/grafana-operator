c6e3ca337edd113102e06dc52b64fee8befb6a05
Igor Beliakov <demtis.register@gmail.com>
2024-05-20 11:21:06 +0200

    fix(controllers): replace flaky e2e test for secureJsonData with a go test

---
+++ controllers/datasource_controller_test.go
@@ -0,0 +1,40 @@
+package controllers
+
+import (
+	"context"
+	"encoding/json"
+	"testing"
+
+	"github.com/grafana/grafana-operator/v5/api/v1beta1"
+	"github.com/stretchr/testify/assert"
+	ctrl "sigs.k8s.io/controller-runtime"
+)
+
+func TestGetDatasourceContent(t *testing.T) {
+	reconciler := &GrafanaDatasourceReconciler{
+		Client: k8sClient,
+		Log:    ctrl.Log.WithName("TestDatasourceReconciler"),
+	}
+
+	t.Run("secureJsonData is preserved", func(t *testing.T) {
+		secureJsonData := []byte(`{"httpHeaderValue1": "Bearer ${PROMETHEUS_TOKEN}"}`)
+		secureJsonDataRaw := (*json.RawMessage)(&secureJsonData)
+		// Here, we only check if contents of secureJsonData is preserved, not secrets replacement
+		want := "Bearer ${PROMETHEUS_TOKEN}"
+
+		cr := &v1beta1.GrafanaDatasource{
+			Spec: v1beta1.GrafanaDatasourceSpec{
+				Datasource: &v1beta1.GrafanaDatasourceInternal{
+					SecureJSONData: *secureJsonDataRaw,
+				},
+			},
+		}
+
+		content, hash, err := reconciler.getDatasourceContent(context.TODO(), cr)
+		got := content.SecureJSONData["httpHeaderValue1"]
+
+		assert.Nil(t, err)
+		assert.NotEmpty(t, hash)
+		assert.Equal(t, want, got)
+	})
+}
