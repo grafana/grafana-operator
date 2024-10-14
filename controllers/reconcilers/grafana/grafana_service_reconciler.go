53b9c176b12496a04bf14c706c7f57f9eda56d99
Dominik Süß <dominik@suess.wtf>
2024-10-14 11:23:44 +0200

    docs: add reasoning for svc suffix

--- controllers/reconcilers/grafana/grafana_service_reconciler.go
+++ controllers/reconcilers/grafana/grafana_service_reconciler.go
@@ -49,6 +49,7 @@ func (r *ServiceReconciler) Reconcile(ctx context.Context, cr *v1beta1.Grafana, 
 
 	// try to assign the admin url
 	if !cr.PreferIngress() {
+		// .svc suffix needed for automatic openshift certificates: https://docs.openshift.com/container-platform/4.17/security/certificates/service-serving-certificate.html#add-service-certificate_service-serving-certificate
 		status.AdminUrl = fmt.Sprintf("%v://%v.%v.svc:%d", getGrafanaServerProtocol(cr), service.Name, cr.Namespace,
 			int32(GetGrafanaPort(cr)))
 	}
