d2b665a5f2838a5271b39bd98fdefa063baff58d
chenlujjj <953546398@qq.com>
2024-11-19 00:51:33 +0800

    feat: support notification template
    chore: pass linter

--- main.go
+++ main.go
@@ -257,6 +257,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "GrafanaNotificationPolicy")
 		os.Exit(1)
 	}
+	if err = (&controllers.GrafanaNotificationTemplateReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "GrafanaNotificationTemplate")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
