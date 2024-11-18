805c3f1d6741e0be1fd2a1ec45979f25fb13d144
ste <steff.bpoulsen@gmail.com>
2024-11-18 17:31:03 +0100

    feat: New common struct

--- api/v1beta1/common.go
+++ api/v1beta1/common.go
@@ -1,6 +1,9 @@
 package v1beta1
 
-import v1 "k8s.io/api/core/v1"
+import (
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
 
 type ValueFrom struct {
 	TargetPath string          `json:"targetPath"`
@@ -15,4 +18,24 @@ type ValueFromSource struct {
 	// Selects a key of a Secret.
 	// +optional
 	SecretKeyRef *v1.SecretKeySelector `json:"secretKeyRef,omitempty"`
+}
+
+// Common Options that all CRs should embed, excluding GrafanaSpec
+// Ensure alignment on handling ResyncPeriod, InstanceSelector, and AllowCrossNamespaceImport
+type GrafanaCommonSpec struct {
+	// How often the resource is synced, defaults to 10m0s if not set
+	// +optional
+	// +kubebuilder:validation:Type=string
+	// +kubebuilder:validation:Format=duration
+	// +kubebuilder:validation:Pattern="^([0-9]+(\\.[0-9]+)?(ns|us|µs|ms|s|m|h))+$"
+	// +kubebuilder:default="10m0s"
+	ResyncPeriod metav1.Duration `json:"resyncPeriod,omitempty"`
+
+	// Selects Grafana instances for import
+	// +kubebuilder:validation:XValidation:rule="self == oldSelf",message="spec.instanceSelector is immutable"
+	InstanceSelector *metav1.LabelSelector `json:"instanceSelector"`
+
+	// Allow the Operator to match this resource with Grafanas outside the current namespace
+	// +optional
+	AllowCrossNamespaceImport *bool `json:"allowCrossNamespaceImport,omitempty"`
 }
