e330b0606a2e310073d8f24f93ce9e8d222ce554
ste <steff.bpoulsen@gmail.com>
2025-01-09 18:10:43 +0100

    fix: Log valuesFrom overrides with debug verbosity

--- controllers/grafanacontactpoint_controller.go
+++ controllers/grafanacontactpoint_controller.go
@@ -205,7 +205,7 @@ func (r *GrafanaContactPointReconciler) buildSettings(ctx context.Context, conta
 		if err != nil {
 			return nil, fmt.Errorf("getting referenced value: %w", err)
 		}
-		r.Log.Info("overriding value", "key", override.TargetPath, "value", val)
+		r.Log.V(1).Info("overriding value", "key", override.TargetPath, "value", val)
 
 		simpleContent.SetPath(strings.Split(override.TargetPath, "."), val)
 	}
