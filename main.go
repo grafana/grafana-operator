3fe566d53af859e40ddd09fba31adce95692d6df
Marius Svechla <m.svechla@gmail.com>
2025-01-24 15:36:41 +0100

    fix

--- main.go
+++ main.go
@@ -257,13 +257,6 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "GrafanaNotificationPolicy")
 		os.Exit(1)
 	}
-	if err = (&controllers.GrafanaNotificationPolicyRouteReconciler{
-		Client: mgr.GetClient(),
-		Scheme: mgr.GetScheme(),
-	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "GrafanaNotificationPolicyRoute")
-		os.Exit(1)
-	}
 	if err = (&controllers.GrafanaNotificationTemplateReconciler{
 		Client: mgr.GetClient(),
 		Scheme: mgr.GetScheme(),
