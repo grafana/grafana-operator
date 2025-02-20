cd67356f04729f25b3a59116e875680e8ed3e50f
Wilfried Roset <wilfriedroset@users.noreply.github.com>
2025-02-20 22:30:42 +0100

    Fix typo
    Signed-off-by: Wilfried Roset <wilfriedroset@users.noreply.github.com>

--- main.go
+++ main.go
@@ -130,7 +130,7 @@ func main() {
 	watchNamespace, _ := os.LookupEnv(watchNamespaceEnvVar)
 	watchNamespaceSelector, _ := os.LookupEnv(watchNamespaceEnvSelector)
 	if watchNamespaceSelector != "" {
-		setupLog.Info(fmt.Sprintf("sharding is enabled via %s=%s. Beware: Always label Grafana CRs before enabling to ensure labels are inherited. Existing Secrets/ConfigMaps referenced in CRs also need to be labelled to continue working.", watchNamespaceEnvSelector, watchNamespaceSelector))
+		setupLog.Info(fmt.Sprintf("sharding is enabled via %s=%s. Beware: Always label Grafana CRs before enabling to ensure labels are inherited. Existing Secrets/ConfigMaps referenced in CRs also need to be labeled to continue working.", watchNamespaceEnvSelector, watchNamespaceSelector))
 	}
 	watchLabelSelectors, _ := os.LookupEnv(watchLabelSelectorsEnvVar)
 
