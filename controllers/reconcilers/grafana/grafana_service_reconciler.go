1195cbea359627b68a6276d453fe18189e31614e
Dominik Süß <dominik@suess.wtf>
2025-02-21 09:11:11 +0100

    fix: headless labels after rebase

--- controllers/reconcilers/grafana/grafana_service_reconciler.go
+++ controllers/reconcilers/grafana/grafana_service_reconciler.go
@@ -57,7 +57,7 @@ func (r *ServiceReconciler) Reconcile(ctx context.Context, cr *v1beta1.Grafana, 
 	// Headless service for grafana unified alerting
 	headlessService := model.GetGrafanaHeadlessService(cr, scheme)
 	_, err = controllerutil.CreateOrUpdate(ctx, r.client, headlessService, func() error {
-		model.SetCommonLabels(headlessService)
+		model.SetInheritedLabels(headlessService, cr.Labels)
 		headlessService.Spec = v1.ServiceSpec{
 			ClusterIP: "None",
 			Ports:     getHeadlessServicePorts(cr),
