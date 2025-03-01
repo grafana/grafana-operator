2264a9d73d637d325eb108c4efe4d5848c1a07e5
ste <steff.bpoulsen@gmail.com>
2025-03-01 10:19:02 +0100

    refactor: Merge external instances into the same reconcile loop
    chore: Clean up getVersion error messages

--- controllers/grafana_controller.go
+++ controllers/grafana_controller.go
@@ -77,42 +77,32 @@ func (r *GrafanaReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ct
 		}
 	}()
 
-	vars := &grafanav1beta1.OperatorReconcileVars{}
+	var stages []grafanav1beta1.OperatorStageName
 	if cr.IsExternal() {
-		cr.Status.Stage = grafanav1beta1.OperatorStageComplete
+		cr.Status.AdminUrl = cr.Spec.External.URL
+		// Only reconcile the Completion stage for external instances
+		stages = []grafanav1beta1.OperatorStageName{grafanav1beta1.OperatorStageComplete}
+	} else {
+		stages = getInstallationStages()
 
-		reconciler := grafana.NewCompleteReconciler(r.Client)
-		stageStatus, err := reconciler.Reconcile(ctx, cr, vars, r.Scheme)
-		if err != nil {
-			cr.Status.StageStatus = stageStatus // Success or failed, both accompanied by Error
-			cr.Status.LastMessage = err.Error()
-			metrics.GrafanaFailedReconciles.WithLabelValues(cr.Name, string(cr.Status.Stage)).Inc()
-
-			return ctrl.Result{}, fmt.Errorf("reconciler error in stage '%s': %w", cr.Status.Stage, err)
-		}
-
-		cr.Status.StageStatus = stageStatus
-		cr.Status.LastMessage = ""
-
-		return ctrl.Result{}, nil
-	}
-
-	// set spec to the current default version to avoid accidental updates when we
-	// change the default. For clusters where RELATED_IMAGE_GRAFANA is set to an
-	// image hash, we want to set this to the value of the variable to support air
-	// gapped clusters as well
-	if cr.Spec.Version == "" {
-		targetVersion := config.GrafanaVersion
-		if envVersion := os.Getenv("RELATED_IMAGE_GRAFANA"); isImageSHA256(envVersion) {
-			targetVersion = envVersion
-		}
-		cr.Spec.Version = targetVersion
-		if err := r.Client.Update(ctx, cr); err != nil {
-			return ctrl.Result{}, fmt.Errorf("updating grafana version in spec: %w", err)
+		// set spec to the current default version to avoid accidental updates when we
+		// change the default. For clusters where RELATED_IMAGE_GRAFANA is set to an
+		// image hash, we want to set this to the value of the variable to support air
+		// gapped clusters as well
+		if cr.Spec.Version == "" {
+			targetVersion := config.GrafanaVersion
+			if envVersion := os.Getenv("RELATED_IMAGE_GRAFANA"); isImageSHA256(envVersion) {
+				targetVersion = envVersion
+			}
+			cr.Spec.Version = targetVersion
+			if err := r.Client.Update(ctx, cr); err != nil {
+				return ctrl.Result{}, fmt.Errorf("updating grafana version in spec: %w", err)
+			}
 		}
 	}
 
-	for _, stage := range getInstallationStages() {
+	vars := &grafanav1beta1.OperatorReconcileVars{}
+	for _, stage := range stages {
 		log.Info("running stage", "stage", stage)
 
 		cr.Status.Stage = stage
@@ -132,9 +122,10 @@ func (r *GrafanaReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ct
 			return ctrl.Result{}, fmt.Errorf("reconciler error in stage '%s': %w", stage, err)
 		}
 
-		cr.Status.StageStatus = stageStatus
-		cr.Status.LastMessage = ""
+		cr.Status.StageStatus = stageStatus // Should always be Success
 	}
+
+	cr.Status.LastMessage = ""
 
 	return ctrl.Result{}, nil
 }
