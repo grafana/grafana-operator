f4773bf7b70d2a8f642e6ca9ac1a43dd1c568a9c
ste <steff.bpoulsen@gmail.com>
2025-03-24 22:46:38 +0100

    chore: Add comment explaining success even when getVersion errors

--- controllers/grafana_controller.go
+++ controllers/grafana_controller.go
@@ -137,6 +137,10 @@ func (r *GrafanaReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ct
 		}
 	}
 
+	// NOTE When exiting the loop successfully, consider the internal instance reconciled
+	// This is due to the 'E2E conditions test' relying on it having a successful status to even attempt reconciliation.
+	// Unready instances are omitted from synchronization
+	// TODO Implement a solution to mark an instance as failing but also validate `ApplyFailed` conditions for resources
 	grafana.Status.StageStatus = grafanav1beta1.OperatorStageResultSuccess
 
 	version, err := r.getVersion(ctx, grafana)
