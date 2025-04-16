3b5208de816c7e1caa866d2fa9165c1c12e9540a
lexhuismans <43178421+lexhuismans@users.noreply.github.com>
2025-04-16 16:27:15 +0200

    Unit test for syncDatasources (#1920)
    * additionally match with ID when looking for datasources cr's
    
    * revert matching with id when finding datasource
    
    * refactor: use function based test
    
    ---------
    
    Co-authored-by: Dominik Süß <dominik@suess.wtf>

--- controllers/datasource_controller_test.go
+++ controllers/datasource_controller_test.go
@@ -5,8 +5,10 @@ import (
 	"encoding/json"
 	"testing"
 
-	"github.com/grafana/grafana-operator/v5/api/v1beta1"
 	"github.com/stretchr/testify/assert"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	v1beta1 "github.com/grafana/grafana-operator/v5/api/v1beta1"
 )
 
 func TestGetDatasourceContent(t *testing.T) {
@@ -35,4 +37,60 @@ func TestGetDatasourceContent(t *testing.T) {
 		assert.NotEmpty(t, hash)
 		assert.Equal(t, want, got)
 	})
+}
+
+func TestGetDatasourcesToDelete(t *testing.T) {
+	f := func(ds []v1beta1.GrafanaDatasource, grafana v1beta1.Grafana, expected []v1beta1.NamespacedResource) {
+		t.Helper()
+		dashboardList := v1beta1.GrafanaDatasourceList{
+			TypeMeta: metav1.TypeMeta{},
+			ListMeta: metav1.ListMeta{},
+			Items: []v1beta1.GrafanaDatasource{
+				{
+					TypeMeta: metav1.TypeMeta{},
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "datasource-a",
+						Namespace: "namespace",
+					},
+					Status: v1beta1.GrafanaDatasourceStatus{
+						UID: "aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa",
+					},
+				},
+			},
+		}
+		datasourcesToDelete := getDatasourcesToDelete(&dashboardList, []v1beta1.Grafana{grafana})
+		for _, out := range datasourcesToDelete {
+			assert.Equal(t, out, expected)
+		}
+	}
+
+	f([]v1beta1.GrafanaDatasource{
+		{
+			TypeMeta: metav1.TypeMeta{},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "datasource-a",
+				Namespace: "namespace",
+			},
+			Status: v1beta1.GrafanaDatasourceStatus{
+				UID: "aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa",
+			},
+		},
+	},
+		v1beta1.Grafana{
+			TypeMeta: metav1.TypeMeta{},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "grafana-1",
+				Namespace: "namespace",
+			},
+			Status: v1beta1.GrafanaStatus{
+				Datasources: v1beta1.NamespacedResourceList{
+					"namespace/datasource-a/aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa",
+					"namespace/datasource-c/cccccccc-cccc-cccc-cccc-cccccccccccc",
+				},
+			},
+		},
+		[]v1beta1.NamespacedResource{
+			"namespace/datasource-c/cccccccc-cccc-cccc-cccc-cccccccccccc",
+		},
+	)
 }
