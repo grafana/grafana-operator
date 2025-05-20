d879a9ade0c827ccbac88dbd914533ada5338215
Steffen Baarsgaard <steff.bpoulsen@gmail.com>
2025-05-20 22:42:32 +0200

    chore: Address unused and errcheck lints

--- api/v1beta1/suite_test.go
+++ api/v1beta1/suite_test.go
@@ -23,7 +23,6 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"k8s.io/client-go/kubernetes/scheme"
-	"k8s.io/client-go/rest"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
@@ -39,7 +38,6 @@ const (
 )
 
 var (
-	cfg       *rest.Config
 	k8sClient client.Client
 	testEnv   *envtest.Environment
 )
