e60e31a3a9aec004cfc5d208a5e7226b6195af30
Igor Beliakov <demtis.register@gmail.com>
2025-05-24 17:03:16 +0200

    chore: modernize maps calls

--- api/v1beta1/typeoverrides.go
+++ api/v1beta1/typeoverrides.go
@@ -3,6 +3,7 @@ package v1beta1
 import (
 	"encoding/json"
 	"fmt"
+	"maps"
 	"reflect"
 
 	"k8s.io/apimachinery/pkg/util/intstr"
@@ -31,17 +32,13 @@ func (override *ObjectMeta) Merge(meta metav1.ObjectMeta) metav1.ObjectMeta {
 		if meta.Annotations == nil {
 			meta.Annotations = make(map[string]string)
 		}
-		for key, val := range override.Annotations {
-			meta.Annotations[key] = val
-		}
+		maps.Copy(meta.Annotations, override.Annotations)
 	}
 	if len(override.Labels) > 0 {
 		if meta.Labels == nil {
 			meta.Labels = make(map[string]string)
 		}
-		for key, val := range override.Labels {
-			meta.Labels[key] = val
-		}
+		maps.Copy(meta.Labels, override.Labels)
 	}
 	return meta
 }
