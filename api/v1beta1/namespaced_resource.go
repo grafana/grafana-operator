eb950cf20c958b690bb0913f356d807b6c622131
Steffen Baarsgaard <steff.bpoulsen@gmail.com>
2025-07-06 20:10:55 +0200

    chore(NamespacedResourceList): Refactor Find, IndexOf, and Delete to use slices.IndexFunc
    Co-authored-by: weisdd <46579601+weisdd@users.noreply.github.com>

--- api/v1beta1/namespaced_resource.go
+++ api/v1beta1/namespaced_resource.go
@@ -2,6 +2,7 @@ package v1beta1
 
 import (
 	"fmt"
+	"slices"
 	"strings"
 )
 
@@ -24,33 +25,34 @@ func (in NamespacedResource) Split() (string, string, string) {
 }
 
 func (in NamespacedResourceList) Find(namespace string, name string) (bool, *string) {
-	for _, r := range in {
-		foundNamespace, foundName, foundUID := r.Split()
-		if foundNamespace == namespace && foundName == name {
-			return true, &foundUID
-		}
+	i := in.IndexOf(namespace, name)
+
+	if i == -1 {
+		return false, nil
 	}
-	return false, nil
+
+	_, _, uid := in[i].Split()
+	return true, &uid
 }
 
 func (in NamespacedResourceList) IndexOf(namespace string, name string) int {
-	for i, r := range in {
-		foundNamespace, foundName, _ := r.Split()
-		if foundNamespace == namespace && foundName == name {
-			return i
-		}
-	}
-	return -1
+	i := slices.IndexFunc(in, func(r NamespacedResource) bool {
+		p := fmt.Sprintf("%s/%s/", namespace, name)
+		return strings.HasPrefix(string(r), p)
+	})
+
+	return i
 }
 
 func (in NamespacedResourceList) Remove(namespace string, name string) NamespacedResourceList {
-	resources := NamespacedResourceList{}
-	for _, r := range in {
-		foundNamespace, foundName, _ := r.Split()
-		if foundNamespace == namespace && foundName == name {
-			continue
-		}
-		resources = append(resources, r)
+	i := in.IndexOf(namespace, name)
+
+	if i == -1 {
+		return in
 	}
-	return resources
+
+	// Swapback delete and return slice
+	// Does not preserve order
+	in[i] = in[len(in)-1]
+	return in[:len(in)-1]
 }
