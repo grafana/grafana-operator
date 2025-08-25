5a259e702d5d811ed75ed294a1ab4062f9c38468
Igor Beliakov <demtis.register@gmail.com>
2025-08-25 09:49:48 +0200

    chore(fetchers): simplify TestFetchJsonnet

--- controllers/content/fetchers/jsonnet_fetcher_test.go
+++ controllers/content/fetchers/jsonnet_fetcher_test.go
@@ -1,10 +1,7 @@
 package fetchers
 
 import (
-	"embed"
 	"encoding/json"
-	"errors"
-	"fmt"
 	"os"
 	"reflect"
 	"strings"
@@ -55,83 +52,65 @@ func normalizeAndCompareJSON(json1, json2 []byte) bool {
 
 func TestFetchJsonnet(t *testing.T) {
 	tests := []struct {
-		name          string
-		dashboard     *v1beta1.GrafanaDashboard
-		libsonnet     embed.FS
-		expected      []byte
-		envs          map[string]string
-		expectedError error
+		name    string
+		jsonnet string
+		envs    map[string]string
+		want    []byte
 	}{
 		{
-			name: "Successful Jsonnet Evaluation",
-			dashboard: &v1beta1.GrafanaDashboard{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      "grafanadashboard-jsonnet",
-					Namespace: "grafana",
-				},
-				Spec: v1beta1.GrafanaDashboardSpec{
-					GrafanaContentSpec: v1beta1.GrafanaContentSpec{
-						Jsonnet: string(embeds.TestDashboardEmbed),
-					},
-				},
-				Status: v1beta1.GrafanaDashboardStatus{},
-			},
-			libsonnet:     embeds.GrafonnetEmbed,
-			expected:      embeds.TestDashboardEmbedExpectedJSON,
-			envs:          map[string]string{},
-			expectedError: nil,
+			name:    "Successful Jsonnet Evaluation",
+			jsonnet: string(embeds.TestDashboardEmbed),
+			envs:    map[string]string{},
+			want:    embeds.TestDashboardEmbedExpectedJSON,
 		},
 		{
-			name: "Empty Jsonnet Content",
-			dashboard: &v1beta1.GrafanaDashboard{
-				ObjectMeta: metav1.ObjectMeta{Name: "dashboard-1"},
-				Spec: v1beta1.GrafanaDashboardSpec{
-					GrafanaContentSpec: v1beta1.GrafanaContentSpec{
-						Jsonnet: "",
-					},
-				},
-				Status: v1beta1.GrafanaDashboardStatus{},
-			},
-			libsonnet:     embeds.GrafonnetEmbed,
-			expected:      nil,
-			envs:          map[string]string{},
-			expectedError: errors.New("no jsonnet Content Found, nil or empty string"),
-		},
-		{
-			name: "Successful Jsonnet Evaluation with non-empty envs",
-			dashboard: &v1beta1.GrafanaDashboard{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      "grafanadashboard-jsonnet",
-					Namespace: "grafana",
-				},
-				Spec: v1beta1.GrafanaDashboardSpec{
-					GrafanaContentSpec: v1beta1.GrafanaContentSpec{
-						Jsonnet: string(embeds.TestDashboardEmbedWithEnv),
-					},
-				},
-				Status: v1beta1.GrafanaDashboardStatus{},
-			},
-			libsonnet: embeds.GrafonnetEmbed,
-			expected:  embeds.TestDashboardEmbedWithEnvExpectedJSON,
+			name:    "Successful Jsonnet Evaluation with non-empty envs",
+			jsonnet: string(embeds.TestDashboardEmbedWithEnv),
+			want:    embeds.TestDashboardEmbedWithEnvExpectedJSON,
 			envs: map[string]string{
 				"TEST_ENV": "123",
 			},
-			expectedError: nil,
 		},
 	}
 
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			result, err := FetchJsonnet(test.dashboard, test.envs, test.libsonnet)
-			if fmt.Sprintf("%v", err) != fmt.Sprintf("%v", test.expectedError) {
-				t.Errorf("expected error %v, but got %v", test.expectedError, err)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cr := &v1beta1.GrafanaDashboard{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "grafanadashboard-jsonnet",
+					Namespace: "grafana",
+				},
+				Spec: v1beta1.GrafanaDashboardSpec{
+					GrafanaContentSpec: v1beta1.GrafanaContentSpec{
+						Jsonnet: tt.jsonnet,
+					},
+				},
 			}
 
-			if test.expected != nil && !normalizeAndCompareJSON(test.expected, result) {
-				t.Errorf("expected string %s, but got %s", string(test.expected), string(result))
-			}
+			got, err := FetchJsonnet(cr, tt.envs, embeds.GrafonnetEmbed)
+			require.NoError(t, err)
+
+			assert.JSONEq(t, string(tt.want), string(got))
 		})
 	}
+
+	t.Run("Empty Jsonnet Content", func(t *testing.T) {
+		cr := &v1beta1.GrafanaDashboard{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "grafanadashboard-jsonnet",
+				Namespace: "grafana",
+			},
+			Spec: v1beta1.GrafanaDashboardSpec{
+				GrafanaContentSpec: v1beta1.GrafanaContentSpec{
+					Jsonnet: "",
+				},
+			},
+		}
+
+		got, err := FetchJsonnet(cr, map[string]string{}, embeds.GrafonnetEmbed)
+		assert.Nil(t, got)
+		require.ErrorIs(t, err, errJsonnetNoContent)
+	})
 }
 
 func TestBuildProjectAndFetchJsonnetFrom(t *testing.T) {
