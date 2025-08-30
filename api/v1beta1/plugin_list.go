21a22f0ad4f6d56c38a4712d4463e2fe1b8cb26a
Igor Beliakov <demtis.register@gmail.com>
2025-08-30 19:18:40 +0200

    chore(plugins): add CRD validation for plugins

--- api/v1beta1/plugin_list.go
+++ api/v1beta1/plugin_list.go
@@ -11,7 +11,10 @@ import (
 )
 
 type GrafanaPlugin struct {
-	Name    string `json:"name"`
+	// +kubebuilder:validation:MinLength=1
+	Name string `json:"name"`
+	// TODO: kubernetes 1.34+ supports isSemver function, we should migrate to it after 1.33 reaches EOL. For now, using the official pattern https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
+	// +kubebuilder:validation:Pattern=`^((0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?|latest)$`
 	Version string `json:"version"`
 }
 
