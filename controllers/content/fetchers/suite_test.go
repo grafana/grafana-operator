5e781a9cfdc97d8477f3b7e56c58fed6b3bdd503
Igor Beliakov <demtis.register@gmail.com>
2025-11-19 11:41:25 +0100

    chore(content): simplify fetchers suite

--- controllers/content/fetchers/suite_test.go
+++ controllers/content/fetchers/suite_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	grafanav1beta1 "github.com/grafana/grafana-operator/v5/api/v1beta1"
+	"github.com/stretchr/testify/require"
 
 	"k8s.io/client-go/kubernetes/scheme"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -31,27 +32,31 @@ func TestAPIs(t *testing.T) {
 }
 
 var _ = BeforeSuite(func() {
+	t := GinkgoT()
+
 	logf.SetLogger(zap.New(zap.WriteTo(GinkgoWriter), zap.UseDevMode(true)))
 
 	By("bootstrapping test environment")
 	testEnv = &envtest.Environment{}
 
 	cfg, err := testEnv.Start()
-	Expect(err).NotTo(HaveOccurred())
-	Expect(cfg).NotTo(BeNil())
+	require.NoError(t, err)
+	require.NotNil(t, cfg)
 
 	err = grafanav1beta1.AddToScheme(scheme.Scheme)
-	Expect(err).NotTo(HaveOccurred())
+	require.NoError(t, err)
 
 	//+kubebuilder:scaffold:scheme
 
 	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme.Scheme})
-	Expect(err).NotTo(HaveOccurred())
-	Expect(k8sClient).NotTo(BeNil())
+	require.NoError(t, err)
+	require.NotNil(t, k8sClient)
 })
 
 var _ = AfterSuite(func() {
+	t := GinkgoT()
+
 	By("tearing down the test environment")
 	err := testEnv.Stop()
-	Expect(err).NotTo(HaveOccurred())
+	require.NoError(t, err)
 })
