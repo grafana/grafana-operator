0890b5d023b97c885b1da4d944ffa5d177bcbf1b
Igor Beliakov <demtis.register@gmail.com>
2025-11-25 13:14:50 +0100

    chore(tests): refactor folder removal test

--- controllers/e2e_force_delete_folder_test.go
+++ controllers/e2e_force_delete_folder_test.go
@@ -11,131 +11,105 @@ import (
 	"github.com/stretchr/testify/require"
 	v1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	controllerruntime "sigs.k8s.io/controller-runtime"
 
 	. "github.com/onsi/ginkgo/v2"
 )
 
-var _ = Describe("Successfully delete GrafanaFolder with GrafanaAlertRuleGroup referencing it", func() {
+var _ = Describe("Folder reconciler", func() {
 	t := GinkgoT()
 
-	f := &v1beta1.GrafanaFolder{
-		ObjectMeta: metav1.ObjectMeta{
-			Namespace: "default",
-			Name:      "force-delete",
-		},
-		Spec: v1beta1.GrafanaFolderSpec{
-			GrafanaCommonSpec: commonSpecSynchronized,
-			CustomUID:         "force-delete",
-		},
-	}
+	It("successfully deletes folder containing AlertRuleGroup", func() {
+		folder := struct {
+			cr  *v1beta1.GrafanaFolder
+			r   GrafanaFolderReconciler
+			req controllerruntime.Request
+		}{}
 
-	arg := &v1beta1.GrafanaAlertRuleGroup{
-		ObjectMeta: metav1.ObjectMeta{
-			Namespace: "default",
-			Name:      "force-delete",
-		},
-		Spec: v1beta1.GrafanaAlertRuleGroupSpec{
-			GrafanaCommonSpec: commonSpecSynchronized,
-			FolderRef:         f.Name,
-			Interval:          metav1.Duration{Duration: 60 * time.Second},
-			Rules: []v1beta1.AlertRule{
-				{
-					Title:     "TestRule",
-					UID:       "akdj-wonvo",
-					Condition: "A",
-					Data: []*v1beta1.AlertQuery{
-						{
-							RefID:             "A",
-							RelativeTimeRange: nil,
-							DatasourceUID:     "__expr__",
-							Model: &v1.JSON{Raw: []byte(`{
-                                "conditions": [
-                                    {
-                                        "evaluator": {
-                                            "params": [
-                                                0,
-                                                0
-                                            ],
-                                            "type": "gt"
-                                        },
-                                        "operator": {
-                                            "type": "and"
-                                        },
-                                        "query": {
-                                            "params": []
-                                        },
-                                        "reducer": {
-                                            "params": [],
-                                            "type": "avg"
-                                        },
-                                        "type": "query"
-                                    }
-                                ],
-                                "datasource": {
-                                    "name": "Expression",
-                                    "type": "__expr__",
-                                    "uid": "__expr__"
-                                },
-                                "expression": "1 > 0",
-                                "hide": false,
-                                "intervalMs": 1000,
-                                "maxDataPoints": 100,
-                                "refId": "B",
-                                "type": "math"
-                            }`)},
+		folder.cr = &v1beta1.GrafanaFolder{
+			ObjectMeta: metav1.ObjectMeta{
+				Namespace: "default",
+				Name:      "force-delete",
+			},
+			Spec: v1beta1.GrafanaFolderSpec{
+				GrafanaCommonSpec: commonSpecSynchronized,
+				CustomUID:         "force-delete",
+			},
+		}
+		folder.r = GrafanaFolderReconciler{Client: k8sClient, Scheme: k8sClient.Scheme()}
+		folder.req = requestFromMeta(folder.cr.ObjectMeta)
+
+		alertRuleGroup := struct {
+			cr  *v1beta1.GrafanaAlertRuleGroup
+			r   GrafanaAlertRuleGroupReconciler
+			req controllerruntime.Request
+		}{}
+
+		alertRuleGroup.cr = &v1beta1.GrafanaAlertRuleGroup{
+			ObjectMeta: metav1.ObjectMeta{
+				Namespace: "default",
+				Name:      "force-delete",
+			},
+			Spec: v1beta1.GrafanaAlertRuleGroupSpec{
+				GrafanaCommonSpec: commonSpecSynchronized,
+				FolderRef:         folder.cr.Name,
+				Interval:          metav1.Duration{Duration: 60 * time.Second},
+				Rules: []v1beta1.AlertRule{
+					{
+						Title:     "TestRule",
+						UID:       "force-delete",
+						Condition: "A",
+						Data: []*v1beta1.AlertQuery{
+							{
+								RefID:         "A",
+								DatasourceUID: "__expr__",
+								Model:         &v1.JSON{Raw: []byte(`{"expression": "1", "refId": "A"}`)},
+							},
 						},
+						ExecErrState: "Error",
+						NoDataState:  ptr.To("NoData"),
 					},
-					NoDataState:  ptr.To("NoData"),
-					ExecErrState: "Error",
-					For:          ptr.To("60s"),
-					Annotations:  map[string]string{},
-					Labels:       map[string]string{},
-					IsPaused:     true,
 				},
 			},
-		},
-	}
+		}
+		alertRuleGroup.r = GrafanaAlertRuleGroupReconciler{Client: k8sClient, Scheme: k8sClient.Scheme()}
+		alertRuleGroup.req = requestFromMeta(alertRuleGroup.cr.ObjectMeta)
 
-	It("Creates folder and rule group, deletes folder, checks folder got deleted", func() {
-		err := k8sClient.Create(testCtx, f)
+		grafanaClient, err := grafanaclient.NewGeneratedGrafanaClient(testCtx, k8sClient, externalGrafanaCr)
 		require.NoError(t, err)
 
-		err = k8sClient.Create(testCtx, arg)
+		// Create folder
+		err = k8sClient.Create(testCtx, folder.cr)
 		require.NoError(t, err)
 
-		By("Reconcile Folder")
-		req := requestFromMeta(f.ObjectMeta)
-		fr := GrafanaFolderReconciler{Client: k8sClient, Scheme: k8sClient.Scheme()}
-
-		_, err = fr.Reconcile(testCtx, req)
+		_, err = folder.r.Reconcile(testCtx, folder.req)
 		require.NoError(t, err)
 
-		By("Reconcile AlertRuleGroup")
-		req = requestFromMeta(arg.ObjectMeta)
-		argr := GrafanaAlertRuleGroupReconciler{Client: k8sClient, Scheme: k8sClient.Scheme()}
-
-		_, err = argr.Reconcile(testCtx, req)
+		// Create AlertRuleGroup
+		err = k8sClient.Create(testCtx, alertRuleGroup.cr)
 		require.NoError(t, err)
 
-		cl, err := grafanaclient.NewGeneratedGrafanaClient(testCtx, k8sClient, externalGrafanaCr)
+		_, err = alertRuleGroup.r.Reconcile(testCtx, alertRuleGroup.req)
 		require.NoError(t, err)
 
-		By("Verifying folder and alert rule group exist")
-		_, err = cl.Folders.GetFolderByUID(f.Spec.CustomUID) //nolint:errcheck
+		// Make sure both resources exist in Grafana
+		uid := folder.cr.Spec.CustomUID
+
+		_, err = grafanaClient.Folders.GetFolderByUID(uid) //nolint:errcheck
 		require.NoErrorf(t, err, "Folder should exist in Grafana")
 
-		_, err = cl.Provisioning.GetAlertRuleGroup(arg.GroupName(), f.Spec.CustomUID) //nolint:errcheck
+		_, err = grafanaClient.Provisioning.GetAlertRuleGroup(alertRuleGroup.cr.GroupName(), uid) //nolint:errcheck
 		require.NoErrorf(t, err, "AlertRuleGroup should exist in Grafana")
 
-		By("Deleting folder")
-		err = k8sClient.Delete(testCtx, f)
+		// Deleting folder
+		err = k8sClient.Delete(testCtx, folder.cr)
 		require.NoError(t, err)
 
-		_, err = fr.Reconcile(testCtx, req)
+		_, err = folder.r.Reconcile(testCtx, folder.req)
 		require.NoError(t, err)
 
-		By("Verifying folder is missing")
-		_, err = cl.Folders.GetFolderByUID(f.Spec.CustomUID) //nolint:errcheck
+		// Make sure the folder is gone
+		_, err = grafanaClient.Folders.GetFolderByUID(uid) //nolint:errcheck
 		require.Error(t, err)
 		assert.IsType(t, &folders.GetFolderByUIDNotFound{}, err)
 	})
