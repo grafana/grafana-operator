f5927036329ddbd2d9a1aa937816c653a64d3e5e
Steffen Baarsgaard <steff.bpoulsen@gmail.com>
2025-11-25 16:15:15 +0100

    chore(imports): metav1

--- api/v1beta1/grafanadashboard_types_test.go
+++ api/v1beta1/grafanadashboard_types_test.go
@@ -7,7 +7,7 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func TestGrafanaStatusListDashboard(t *testing.T) {
@@ -21,17 +21,17 @@ func TestGrafanaStatusListDashboard(t *testing.T) {
 
 func newDashboard(name string, uid string) *GrafanaDashboard {
 	return &GrafanaDashboard{
-		TypeMeta: v1.TypeMeta{
+		TypeMeta: metav1.TypeMeta{
 			APIVersion: APIVersion,
 			Kind:       "GrafanaDashboard",
 		},
-		ObjectMeta: v1.ObjectMeta{
+		ObjectMeta: metav1.ObjectMeta{
 			Name:      name,
 			Namespace: "default",
 		},
 		Spec: GrafanaDashboardSpec{
 			GrafanaCommonSpec: GrafanaCommonSpec{
-				InstanceSelector: &v1.LabelSelector{
+				InstanceSelector: &metav1.LabelSelector{
 					MatchLabels: map[string]string{
 						"test": "datasource",
 					},
