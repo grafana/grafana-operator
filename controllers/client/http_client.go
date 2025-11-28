7ccd2c3d7023ccbd75cde05a8bd814144b975ce3
Igor Beliakov <demtis.register@gmail.com>
2025-11-28 14:27:37 +0100

    chore(controllers): move version discovery from reconciler to client

--- controllers/client/http_client.go
+++ controllers/client/http_client.go
@@ -2,6 +2,8 @@ package client
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"time"
 
@@ -36,4 +38,48 @@ func NewHTTPClient(ctx context.Context, c client.Client, cr *v1beta1.Grafana) (*
 		Transport: transport,
 		Timeout:   time.Second * timeout,
 	}, nil
+}
+
+func GetGrafanaVersion(ctx context.Context, c client.Client, cr *v1beta1.Grafana) (string, error) {
+	httpClient, err := NewHTTPClient(ctx, c, cr)
+	if err != nil {
+		return "", fmt.Errorf("setup of the http client: %w", err)
+	}
+
+	gURL, err := ParseAdminURL(cr.Status.AdminURL)
+	if err != nil {
+		return "", err
+	}
+
+	instanceURL := gURL.JoinPath("/frontend/settings").String()
+
+	req, err := http.NewRequest(http.MethodGet, instanceURL, nil)
+	if err != nil {
+		return "", fmt.Errorf("building request to fetch version: %w", err)
+	}
+
+	err = InjectAuthHeaders(ctx, c, cr, req)
+	if err != nil {
+		return "", fmt.Errorf("fetching credentials for version detection: %w", err)
+	}
+
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		return "", err
+	}
+
+	data := struct {
+		BuildInfo struct {
+			Version string `json:"version"`
+		} `json:"buildInfo"`
+	}{}
+	if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
+		return "", fmt.Errorf("parsing health endpoint data: %w", err)
+	}
+
+	if data.BuildInfo.Version == "" {
+		return "", fmt.Errorf("empty version received from server")
+	}
+
+	return data.BuildInfo.Version, nil
 }
