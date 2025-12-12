70afdff16befc5eefa4e999158dd1cc04f3bb5f2
Steffen Baarsgaard <steff.bpoulsen@gmail.com>
2025-12-12 19:05:55 +0100

    feat(Grafana): Explicitly authenticate before getting version

--- controllers/client/http_client.go
+++ controllers/client/http_client.go
@@ -83,3 +83,50 @@ func GetGrafanaVersion(ctx context.Context, c client.Client, cr *v1beta1.Grafana
 
 	return data.BuildInfo.Version, nil
 }
+
+func GetAuthenticationStatus(ctx context.Context, c client.Client, cr *v1beta1.Grafana) (bool, error) {
+	httpClient, err := NewHTTPClient(ctx, c, cr)
+	if err != nil {
+		return false, fmt.Errorf("setup of the http client: %w", err)
+	}
+
+	gURL, err := ParseAdminURL(cr.Status.AdminURL)
+	if err != nil {
+		return false, err
+	}
+
+	instanceURL := gURL.JoinPath("/login/ping").String()
+
+	req, err := http.NewRequest(http.MethodGet, instanceURL, nil)
+	if err != nil {
+		return false, fmt.Errorf("building request to fetch authentication status: %w", err)
+	}
+
+	err = InjectAuthHeaders(ctx, c, cr, req)
+	if err != nil {
+		return false, fmt.Errorf("fetching credentials for authentication: %w", err)
+	}
+
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		return false, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return false, fmt.Errorf("failed to authenticate with grafana, ensure connectivity and valid credentials")
+	}
+
+	data := struct {
+		Message string `json:"message"`
+	}{}
+	if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
+		return false, fmt.Errorf("parsing health endpoint data: %w", err)
+	}
+
+	// The endpoint response has never changed since being introduced on 2015-04-07 in 22adf0d06e891a555d9ec40ec09f89d6679bafec (Grafana)
+	if data.Message != "Logged in" {
+		return false, fmt.Errorf("unexpected api response, expected: {\"message\": \"Logged in\"}, got: %v", data)
+	}
+
+	return true, nil
+}
