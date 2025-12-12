c328a1e5a32e3d7343ac49a9213fe5e1698e7f5f
Steffen Baarsgaard <steff.bpoulsen@gmail.com>
2025-12-12 18:43:48 +0100

    chore(client): Deduplicate client instantiation

--- controllers/client/grafana_client.go
+++ controllers/client/grafana_client.go
@@ -3,27 +3,16 @@ package client
 import (
 	"context"
 	"fmt"
-	"net/http"
 	"net/url"
-	"time"
 
 	httptransport "github.com/go-openapi/runtime/client"
 	genapi "github.com/grafana/grafana-openapi-client-go/client"
 	"github.com/grafana/grafana-operator/v5/api/v1beta1"
-	"github.com/grafana/grafana-operator/v5/controllers/metrics"
-	"github.com/prometheus/client_golang/prometheus"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 func NewGeneratedGrafanaClient(ctx context.Context, c client.Client, cr *v1beta1.Grafana) (*genapi.GrafanaHTTPAPI, error) {
-	var timeout time.Duration
-	if cr.Spec.Client != nil && cr.Spec.Client.TimeoutSeconds != nil {
-		timeout = max(time.Duration(*cr.Spec.Client.TimeoutSeconds), 0)
-	} else {
-		timeout = 10
-	}
-
-	tlsConfig, err := buildTLSConfiguration(ctx, c, cr)
+	httpClient, err := NewHTTPClient(ctx, c, cr)
 	if err != nil {
 		return nil, err
 	}
@@ -31,14 +20,6 @@ func NewGeneratedGrafanaClient(ctx context.Context, c client.Client, cr *v1beta1
 	gURL, err := ParseAdminURL(cr.Status.AdminURL)
 	if err != nil {
 		return nil, err
-	}
-
-	transport := NewInstrumentedRoundTripper(cr.IsExternal(), tlsConfig, metrics.GrafanaAPIRequests.MustCurryWith(prometheus.Labels{
-		"instance_namespace": cr.Namespace,
-		"instance_name":      cr.Name,
-	}))
-	if cr.Spec.Client != nil && cr.Spec.Client.Headers != nil {
-		transport.(*instrumentedRoundTripper).addHeaders(cr.Spec.Client.Headers) //nolint:errcheck
 	}
 
 	// Secrets and ConfigMaps are not cached by default, get credentials as the last step.
@@ -55,11 +36,7 @@ func NewGeneratedGrafanaClient(ctx context.Context, c client.Client, cr *v1beta1
 		APIKey: credentials.apikey,
 		// NumRetries contains the optional number of attempted retries
 		NumRetries: 0,
-		TLSConfig:  tlsConfig,
-		Client: &http.Client{
-			Transport: transport,
-			Timeout:   timeout * time.Second,
-		},
+		Client:     httpClient,
 	}
 	if credentials.adminUser != "" {
 		cfg.BasicAuth = url.UserPassword(credentials.adminUser, credentials.adminPassword)
