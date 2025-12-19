fc1e5e276593da9594add2ed27120df767abb338
Igor Beliakov <demtis.register@gmail.com>
2025-12-19 11:24:43 +0100

    feat(tk8s): add GetRequest

---
+++ pkg/tk8s/resources.go
@@ -0,0 +1,21 @@
+package tk8s
+
+import (
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	ctrl "sigs.k8s.io/controller-runtime"
+)
+
+func GetRequest(t tHelper, obj client.Object) ctrl.Request {
+	t.Helper()
+
+	req := ctrl.Request{
+		NamespacedName: types.NamespacedName{
+			Name:      obj.GetName(),
+			Namespace: obj.GetNamespace(),
+		},
+	}
+
+	return req
+}
