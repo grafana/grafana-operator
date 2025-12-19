5ad52cca3e5bf410027579c7fc646ddb1410fe77
Igor Beliakov <demtis.register@gmail.com>
2025-12-19 09:49:18 +0100

    feat(tests): add tk8s package

---
+++ pkg/tk8s/env.go
@@ -0,0 +1,22 @@
+package tk8s
+
+import (
+	"testing"
+
+	corev1 "k8s.io/api/core/v1"
+)
+
+func GetEnvVarSecretSource(t *testing.T, secretName, key string) *corev1.EnvVarSource {
+	t.Helper()
+
+	v := &corev1.EnvVarSource{
+		SecretKeyRef: &corev1.SecretKeySelector{
+			LocalObjectReference: corev1.LocalObjectReference{
+				Name: secretName,
+			},
+			Key: key,
+		},
+	}
+
+	return v
+}
