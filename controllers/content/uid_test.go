87142c40a05708dc4f6421643b8b6666fa825be9
Igor Beliakov <demtis.register@gmail.com>
2025-12-27 11:30:57 +0100

    chore(content): improve naming for TestIsUpdatedUID

--- controllers/content/uid_test.go
+++ controllers/content/uid_test.go
@@ -12,141 +12,143 @@ import (
 )
 
 func TestIsUpdatedUID(t *testing.T) {
-	crUID := "crUID"
-	dashUID := "dashUID"
-	specUID := "specUID"
+	const (
+		contentUID = "contentUID"
+		crUID      = "crUID"
+		specUID    = "specUID"
+	)
+
 	tests := []struct {
-		name         string
-		crUID        string
-		statusUID    string
-		dashboardUID string
-		specUID      string
-		want         bool
+		name       string
+		crUID      string
+		statusUID  string
+		contentUID string
+		specUID    string
+		want       bool
 	}{
 		// Validate always false when statusUID is empty
-		// Since dashboardUID is ignoredk the only variable is customUID
 		{
-			name:         "Empty StatusUID always results in false",
-			crUID:        crUID,
-			statusUID:    "",
-			dashboardUID: "",
-			specUID:      "",
-			want:         false,
+			name:       "Empty StatusUID always results in false",
+			crUID:      crUID,
+			statusUID:  "",
+			contentUID: "",
+			specUID:    "",
+			want:       false,
 		},
 		{
-			name:         "Always false when statusUID is empty regardless of dashUID being set",
-			crUID:        crUID,
-			statusUID:    "",
-			dashboardUID: dashUID,
-			specUID:      "",
-			want:         false,
+			name:       "Always false when statusUID is empty regardless of contentUID being set",
+			crUID:      crUID,
+			statusUID:  "",
+			contentUID: contentUID,
+			specUID:    "",
+			want:       false,
 		},
 		{
-			name:         "Always false when statusUID is empty regardless of customUID being set",
-			crUID:        crUID,
-			statusUID:    "",
-			dashboardUID: "",
-			specUID:      specUID,
-			want:         false,
+			name:       "Always false when statusUID is empty regardless of customUID being set",
+			crUID:      crUID,
+			statusUID:  "",
+			contentUID: "",
+			specUID:    specUID,
+			want:       false,
 		},
 		{
-			name:         "Always false when statusUID is empty regardless of customUID or dashUID being set",
-			crUID:        crUID,
-			statusUID:    "",
-			dashboardUID: dashUID,
-			specUID:      specUID,
-			want:         false,
+			name:       "Always false when statusUID is empty regardless of customUID or contentUID being set",
+			crUID:      crUID,
+			statusUID:  "",
+			contentUID: contentUID,
+			specUID:    specUID,
+			want:       false,
 		},
-		// Validate that crUID is always overwritten by dashUID or customUID
-		// dashboardUID is always overwritten by customUID which falls back to crUID
+		// Validate that crUID is always overwritten by contentUID or customUID
+		// contentUID is always overwritten by customUID which falls back to crUID
 		{
-			name:         "DashboardUID and customUID empty",
-			crUID:        crUID,
-			statusUID:    crUID,
-			dashboardUID: "",
-			specUID:      "",
-			want:         false,
-		},
-		{
-			name:         "DashboardUID set and customUID empty",
-			crUID:        crUID,
-			statusUID:    dashUID,
-			dashboardUID: dashUID,
-			specUID:      "",
-			want:         false,
+			name:       "contentUID and customUID empty",
+			crUID:      crUID,
+			statusUID:  crUID,
+			contentUID: "",
+			specUID:    "",
+			want:       false,
 		},
 		{
-			name:         "DashboardUID set and customUID set",
-			crUID:        crUID,
-			statusUID:    specUID,
-			dashboardUID: dashUID,
-			specUID:      specUID,
-			want:         false,
+			name:       "contentUID set and customUID empty",
+			crUID:      crUID,
+			statusUID:  contentUID,
+			contentUID: contentUID,
+			specUID:    "",
+			want:       false,
 		},
 		{
-			name:         "DashboardUID empty and customUID set",
-			crUID:        crUID,
-			statusUID:    specUID,
-			dashboardUID: "",
-			specUID:      specUID,
-			want:         false,
+			name:       "contentUID set and customUID set",
+			crUID:      crUID,
+			statusUID:  specUID,
+			contentUID: contentUID,
+			specUID:    specUID,
+			want:       false,
+		},
+		{
+			name:       "contentUID empty and customUID set",
+			crUID:      crUID,
+			statusUID:  specUID,
+			contentUID: "",
+			specUID:    specUID,
+			want:       false,
 		},
 		// Validate updates are detected correctly
 		{
-			name:         "DashboardUID updated and customUID empty",
-			crUID:        crUID,
-			statusUID:    crUID,
-			dashboardUID: dashUID,
-			specUID:      "",
-			want:         true,
+			name:       "contentUID updated and customUID empty",
+			crUID:      crUID,
+			statusUID:  crUID,
+			contentUID: contentUID,
+			specUID:    "",
+			want:       true,
 		},
 		{
-			name:         "DashboardUID updated and customUID set",
-			crUID:        crUID,
-			statusUID:    specUID,
-			dashboardUID: dashUID,
-			specUID:      specUID,
-			want:         false,
+			name:       "contentUID updated and customUID set",
+			crUID:      crUID,
+			statusUID:  specUID,
+			contentUID: contentUID,
+			specUID:    specUID,
+			want:       false,
 		},
 		{
-			name:         "new dashUID and no customUID",
-			crUID:        crUID,
-			statusUID:    "oldUID",
-			dashboardUID: dashUID,
-			specUID:      "",
-			want:         true,
+			name:       "new contentUID and no customUID",
+			crUID:      crUID,
+			statusUID:  "oldUID",
+			contentUID: contentUID,
+			specUID:    "",
+			want:       true,
 		},
 		{
-			name:         "dashUID removed and no customUID",
-			crUID:        crUID,
-			statusUID:    "oldUID",
-			dashboardUID: "",
-			specUID:      "",
-			want:         true,
+			name:       "contentUID removed and no customUID",
+			crUID:      crUID,
+			statusUID:  "oldUID",
+			contentUID: "",
+			specUID:    "",
+			want:       true,
 		},
 		// Validate that statusUID detection works even in impossible cases expecting cr or customUID to change
 		{
-			name:         "IMPOSSIBLE: Old status with new customUID",
-			crUID:        crUID,
-			statusUID:    "oldUID",
-			dashboardUID: "",
-			specUID:      specUID,
-			want:         true,
+			name:       "IMPOSSIBLE: Old status with new customUID",
+			crUID:      crUID,
+			statusUID:  "oldUID",
+			contentUID: "",
+			specUID:    specUID,
+			want:       true,
 		},
 		{
-			name:         "IMPOSSIBLE: Old Status with all UIDs being equal",
-			crUID:        crUID,
-			statusUID:    "oldUID",
-			dashboardUID: crUID,
-			specUID:      crUID,
-			want:         true,
+			name:       "IMPOSSIBLE: Old Status with all UIDs being equal",
+			crUID:      crUID,
+			statusUID:  "oldUID",
+			contentUID: crUID,
+			specUID:    crUID,
+			want:       true,
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			model := map[string]any{
-				"uid": tt.dashboardUID,
+				"uid": tt.contentUID,
 			}
 
 			dashboard, err := json.Marshal(model)
@@ -169,7 +171,7 @@ func TestIsUpdatedUID(t *testing.T) {
 				},
 			}
 
-			uid := GetGrafanaUID(cr, tt.dashboardUID)
+			uid := GetGrafanaUID(cr, tt.contentUID)
 
 			got := IsUpdatedUID(cr, uid)
 			assert.Equal(t, tt.want, got)
