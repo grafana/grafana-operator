d43866a62f57518c169c9924fe7bdb3de1955cbe
Igor Beliakov <demtis.register@gmail.com>
2026-01-16 23:07:06 +0100

    feat(tk8s): add GetGrafanaTestContainer

---
+++ pkg/tk8s/containers.go
@@ -0,0 +1,27 @@
+package tk8s
+
+import (
+	"context"
+	"time"
+
+	"github.com/stretchr/testify/require"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
+)
+
+func GetGrafanaTestContainer(t tHelper, ctx context.Context, image string) testcontainers.Container {
+	t.Helper()
+
+	c, err := testcontainers.Run(ctx, image,
+		testcontainers.WithExposedPorts(
+			"3000/tcp",
+		),
+		testcontainers.WithAdditionalWaitStrategy(
+			wait.ForHTTP("/").WithStartupTimeout(10*time.Second),
+		),
+	)
+
+	require.NoError(t, err)
+
+	return c
+}
