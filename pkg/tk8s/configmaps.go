dc375b0bc97f47b7139524cb6ec2b0fb4097502c
Igor Beliakov <demtis.register@gmail.com>
2026-04-07 11:58:25 +0200

    feat(tk8s): add GetEnvFromConfigMapSource and GetEnvFromSecretSource

--- pkg/tk8s/configmaps.go
+++ pkg/tk8s/configmaps.go
@@ -2,6 +2,18 @@ package tk8s
 
 import corev1 "k8s.io/api/core/v1"
 
+func GetEnvFromConfigMapSource(t tHelper, configMapName string) *corev1.ConfigMapEnvSource {
+	t.Helper()
+
+	v := &corev1.ConfigMapEnvSource{
+		LocalObjectReference: corev1.LocalObjectReference{
+			Name: configMapName,
+		},
+	}
+
+	return v
+}
+
 func GetEnvVarConfigMapSource(t tHelper, configMapName, key string) *corev1.EnvVarSource {
 	t.Helper()
 
--- pkg/tk8s/secrets.go
+++ pkg/tk8s/secrets.go
@@ -4,6 +4,18 @@ import (
 	corev1 "k8s.io/api/core/v1"
 )
 
+func GetEnvFromSecretSource(t tHelper, secretName string) *corev1.SecretEnvSource {
+	t.Helper()
+
+	v := &corev1.SecretEnvSource{
+		LocalObjectReference: corev1.LocalObjectReference{
+			Name: secretName,
+		},
+	}
+
+	return v
+}
+
 func GetEnvVarSecretSource(t tHelper, secretName, key string) *corev1.EnvVarSource {
 	t.Helper()
 
