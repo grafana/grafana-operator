25e7b5b6fb2821adacbba633f96f0e0422425cdf
Igor Beliakov <46579601+weisdd@users.noreply.github.com>
2026-06-03 01:16:10 +0200

    chore(workflows): update test matrix (#2733)
    * chore(toolchain): bump kind to v0.32.0
    
    * chore(workflows): update test matrix

--- Toolchain.mk
+++ Toolchain.mk
@@ -19,7 +19,7 @@ HELM_DOCS_VERSION = 1.14.2
 HELM_VERSION = v3.17.3
 HUGO_VERSION = 0.151.0
 # renovate: datasource=github-tags depName=kubernetes-sigs/kind
-KIND_VERSION = v0.31.0
+KIND_VERSION = v0.32.0
 # renovate: datasource=github-tags depName=ko-build/ko
 KO_VERSION = 0.18.1
 # renovate: datasource=github-tags depName=kubernetes-sigs/kustomize extractVersion=^kustomize/(?<version>.*)$
